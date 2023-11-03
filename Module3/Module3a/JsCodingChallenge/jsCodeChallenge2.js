ad4a4bde8f59762d08750d2178326233c5baafd1
chioma <chiomapromise169@gmail.com>
2023-11-03 11:53:33 +0100

    add

--- Module3/Module3a/JsCodingChallenge/jsCodeChallenge2.js
+++ Module3/Module3a/JsCodingChallenge/jsCodeChallenge2.js
@@ -1,10 +1,3 @@
-// function converter(millimetre){
-// const metre = millimetre / 1000;
-// return metre;
-// }
-// const metreConverter = converter(7);
-// console.log(metreConverter);
-
 function converter(millimetre){
     const metre = millimetre / 1000;
     return metre;
--- Module3/Module3a/JsCodingChallenge/jsCodeChallenge9.js
+++ Module3/Module3a/JsCodingChallenge/jsCodeChallenge9.js
@@ -1,1 +1,16 @@
-const sentence = 'She is a beautiful girl'
+
+function countVowels(string) {
+    const vowels = 'AEIOUaeiou';
+    let count = 0;
+    
+    for (let i = 0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+const sentence = "She is a beautiful girl";
+const vowelCount = countVowels(sentence);
+console.log("Number of vowels:", vowelCount);

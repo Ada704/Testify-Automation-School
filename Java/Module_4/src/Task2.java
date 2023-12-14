2721c410e502af98e08f101e43e6cb1c53b7b413
chioma <chiomapromise169@gmail.com>
2023-12-14 22:28:49 +0100

    Task 2

---
+++ Java/Module_4/src/Task2.java
@@ -0,0 +1,17 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int number = 10; // Integer variable
+        double decimal = 3.14; // Double variable
+        char character = 'A'; // Character variable
+        boolean flag = true; // Boolean variable
+        String text = "Hello, Java!"; // String variable
+        long largeNumber = 1234567890L; // Long variable
+
+        System.out.println("Integer: " + number);
+        System.out.println("Double: " + decimal);
+        System.out.println("Character: " + character);
+        System.out.println("Boolean: " + flag);
+        System.out.println("String: " + text);
+        System.out.println("Long: " + largeNumber);
+    }
+}

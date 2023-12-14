934baa05316c6256817c03b4fe4a760e1d36d73b
chioma <chiomapromise169@gmail.com>
2023-12-14 22:43:40 +0100

    Task 4

---
+++ Java/Module_4/src/Task4.java
@@ -0,0 +1,9 @@
+public class Task4 {
+    public static void main(String[] args) {
+        double radius = 12.7;
+        double pi = Math.PI;
+        double areaOfCircle = pi * radius * radius;
+
+        System.out.println("The area of the circle with a radius of " + radius + " meters is: " + areaOfCircle + " square meters");
+    }
+}

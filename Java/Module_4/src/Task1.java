32b32e9f5bce7c784d5d8a4c3ba0e522a43f0b9b
chioma <chiomapromise169@gmail.com>
2023-12-14 22:24:25 +0100

    add file

--- Java/Module_4/src/Task1.java
+++ Java/Module_4/src/Task1.java
@@ -1,2 +1,9 @@
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Great God");
+        System.out.println("Chioma");
+        System.out.println("Wealth");
+    }
+
 }

ef559a578f9b83f89fec9ebd37caeea274176b39
chioma <chiomapromise169@gmail.com>
2023-12-14 22:50:27 +0100

    Task 5

---
+++ Java/Module_4/src/Task5.java
@@ -0,0 +1,17 @@
+public class Task5 {
+    public static void main(String[] args) {
+        int n = 55;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println("enter a valid number.");
+            }
+        }
+    }
+}

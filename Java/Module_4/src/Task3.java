8b3a1ff69f960513ab861e06eb584fc3edac6f55
chioma <chiomapromise169@gmail.com>
2023-12-14 22:38:45 +0100

    Task 2

---
+++ Java/Module_4/src/Task3.java
@@ -0,0 +1,10 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int age = 99;
+        String information = "I am  ";
+
+        System.out.println(information + age + "years old");
+        System.out.println(information.concat(age + "years old"));
+
+    }
+}

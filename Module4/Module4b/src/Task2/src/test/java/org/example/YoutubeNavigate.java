17a27dc6707dd1a2f2638ed9d0246b78f456d68d
chioma <chiomapromise169@gmail.com>
2024-01-25 19:00:20 +0100

    task2

---
+++ Module4/Module4b/src/Task2/src/test/java/org/example/YoutubeNavigate.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class YoutubeNavigate {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/user/Downloads/Testify-Automation-School/Module4/Module4b/Task_2/src/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.youtube.com/");
+    }
+}
+

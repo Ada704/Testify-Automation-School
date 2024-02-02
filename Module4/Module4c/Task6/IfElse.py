57206f304f9a5d689a99399b995d58a2ed900f7b
chioma <chiomapromise169@gmail.com>
2024-02-02 11:23:22 +0100

    Task6

---
+++ Module4/Module4c/Task6/IfElse.py
@@ -0,0 +1,12 @@
+
+first_number = 84
+second_number = 56
+best_language = "python"
+
+if first_number > 50 and second_number < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+if best_language == "python":
+    print("Yay!! The string is a Testify Python")

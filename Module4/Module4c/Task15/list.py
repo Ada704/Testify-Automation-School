86588659a1bc3643da8bb2dfed80bd4aa3945990
chioma <chiomapromise169@gmail.com>
2024-02-05 21:36:58 +0100

    Task15

---
+++ Module4/Module4c/Task15/list.py
@@ -0,0 +1,18 @@
+
+continents = ["Europe", "Africa", "North America", "Asia", "Antarctica", "South America", "Ocean"]
+print(continents)
+
+continents.reverse()
+print(continents)
+
+continents.sort()
+print("ascending:", continents)
+
+continents.sort(reverse=True)
+print("desc:", continents)
+
+
+
+
+
+

19def804de4e6d852a499af35393bce702890990
chioma <chiomapromise169@gmail.com>
2024-02-05 23:28:55 +0100

    Task18

---
+++ Module4/Module4c/Task18/class.py
@@ -0,0 +1,11 @@
+
+class Human:
+
+    leg_count = 4
+    can_walk = True
+
+
+instantiate_human = Human()
+
+print("Leg Count:", instantiate_human.leg_count)
+print("Can Walk:", instantiate_human.can_walk)

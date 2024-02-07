db86670f8eaefd48625c7c49952bf4ca518043f5
chioma <chiomapromise169@gmail.com>
2024-02-07 19:38:44 +0100

    Task19

---
+++ Module4/Module4c/Task19/class.py
@@ -0,0 +1,24 @@
+class Human:
+
+    leg_count = 4
+    can_walk = True
+
+
+def __init__(self):
+    self.name = "unknown"
+
+
+def get_description(self):
+    print("This is human")
+
+
+human = Human()
+
+
+def get_leg_count(self, name):
+    return self.leg_count
+
+
+human.get_description()
+
+# print("Leg count:", human.get_leg_count())

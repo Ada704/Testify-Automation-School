1cc1e44ef4db44143aeca0a178b207aaafb27f80
chioma <chiomapromise169@gmail.com>
2024-02-12 11:44:02 +0100

    Task26

---
+++ Module4/Module4c/Task_26/test_compare.py
@@ -0,0 +1,11 @@
+
+def test_number_comparison():
+    num1 = 10
+    num2 = 20
+    assert num1 != num2
+
+
+def test_string_comparison():
+    str1 = "hello"
+    str2 = "hello"
+    assert str1 == str2

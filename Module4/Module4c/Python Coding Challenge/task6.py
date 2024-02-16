35dd50cbc815f96fae1815c113473c18c2694fbf
chioma <chiomapromise169@gmail.com>
2024-02-16 17:41:25 +0100

    Task6

---
+++ Module4/Module4c/Python Coding Challenge/task6.py
@@ -0,0 +1,8 @@
+def sort_strings(arr):
+    return sorted(arr)
+
+
+qualities = ["laughter", "hope", "joy", "cheerful", "sooth"]
+sorted_array = sort_strings(qualities)
+print("Original array:", qualities)
+print("Sorted array:", sorted_array)

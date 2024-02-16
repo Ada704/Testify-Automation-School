f5e14bfe0ae99ed7eb6f515fe95facd7a42b3b17
chioma <chiomapromise169@gmail.com>
2024-02-16 17:41:13 +0100

    Task5

---
+++ Module4/Module4c/Python Coding Challenge/task5.py
@@ -0,0 +1,8 @@
+def reverse_array(arr):
+    return arr[::-1]
+
+
+virtues = ["good", "great", "awesome", "beautiful", "excellence", "positive"]
+reversed_array = reverse_array(virtues)
+print("Original array:", virtues)
+print("Reversed array:", reversed_array)
